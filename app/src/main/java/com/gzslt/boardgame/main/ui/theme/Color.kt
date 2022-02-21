2be8b8e7c88d719defead4aad417de8d163b8e9e
Zsolt <geczy.zsolt@gmail.com>
2022-02-21 14:59:51 +0100

    Move ui package into main package

--- app/src/main/java/com/gzslt/boardgame/main/ui/theme/Color.kt
+++ app/src/main/java/com/gzslt/boardgame/main/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.gzslt.boardgame.ui.theme
+package com.gzslt.boardgame.main.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/gzslt/boardgame/main/ui/theme/Type.kt
+++ app/src/main/java/com/gzslt/boardgame/main/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.gzslt.boardgame.ui.theme
+package com.gzslt.boardgame.main.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
