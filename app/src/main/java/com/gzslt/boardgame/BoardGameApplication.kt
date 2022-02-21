7690f21a47153e3312ff4f89583dd2d5b0807942
Zsolt <geczy.zsolt@gmail.com>
2022-02-21 14:36:13 +0100

    Set up logging

--- app/src/main/java/com/gzslt/boardgame/BoardGameApplication.kt
+++ app/src/main/java/com/gzslt/boardgame/BoardGameApplication.kt
@@ -1,5 +1,13 @@
 package com.gzslt.boardgame
 
 import android.app.Application
+import logcat.AndroidLogcatLogger
+import logcat.LogPriority
 
-class BoardGameApplication : Application()
+class BoardGameApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        AndroidLogcatLogger.installOnDebuggableApp(this, LogPriority.VERBOSE)
+    }
+}
